6f2754760a15591e16b6eae9c521fcc4c85013b6
CarnunMP <carnun@hotmail.co.uk>
2020-02-12 18:16:53 +0000

    Day 1 MVP (+ one or two things) done!

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,6 @@
 from room import Room
+from player import Player
+import textwrap
 
 # Declare all the rooms
 
@@ -24,20 +26,32 @@ earlier adventurers. The only exit is to the south."""),
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+# room['outside'].n_to = room['foyer']
+# room['foyer'].s_to = room['outside']
+# room['foyer'].n_to = room['overlook']
+# room['foyer'].e_to = room['narrow']
+# room['overlook'].s_to = room['foyer']
+# room['narrow'].w_to = room['foyer']
+# room['narrow'].n_to = room['treasure']
+# room['treasure'].s_to = room['narrow']
+
+### I didn't like the above implementation. Went for the following instead:
+room['outside'].n_to = 'foyer'
+room['foyer'].s_to = 'outside'
+room['foyer'].n_to = 'overlook'
+room['foyer'].e_to = 'narrow'
+room['overlook'].s_to = 'foyer'
+room['narrow'].w_to = 'foyer'
+room['narrow'].n_to = 'treasure'
+room['treasure'].s_to = 'narrow'
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
+current_room_key = 'outside'
+player = Player('Carnun', current_room_key)
 
 # Write a loop that:
 #
@@ -49,3 +63,41 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+while current_room_key != 'QUIT':
+    current_room = room[current_room_key]
+
+    print("- - - - - - - - - - - - - - - - - - - - - - \n")
+    print(f'You are in {current_room.name}.')
+    print(textwrap.fill(current_room.description))
+
+    # Todo: make prompt dynamic!
+    available_directions = current_room.get_available_directions()
+    prompt_middle = ""
+    if len(available_directions) == 1:
+        prompt_middle = f"Enter {available_directions[0]}."
+    elif len(available_directions) == 2:
+        prompt_middle = f"Enter {available_directions[0]} or {available_directions[1]}."
+    elif len(available_directions) == 3:
+        prompt_middle = f"Enter {available_directions[0]}, {available_directions[1]}, or {available_directions[2]}."
+    else:
+        prompt_middle = f"Enter {available_directions[0]}, {available_directions[1]}, {available_directions[2]}, or {available_directions[4]}."
+    prompt = f"> Where would you like to go? {prompt_middle} (Or enter 'q' to quit.) \n"
+    user_input = input(prompt).lower()
+
+    if user_input == 'q':
+        print("You have quit the game. Bye!")
+        current_room_key = 'QUIT'
+    elif user_input != 'n' and user_input != 'e' and user_input != 's' and user_input != 'w':
+        print("Please enter one of the cardinal directions. (Or enter 'q' to quit.)")
+    else:
+        try:
+            next_room_key = current_room.enter(user_input)
+            # If this next line fails...
+            current_room = room[next_room_key]
+            # ... this never gets called:
+            current_room_key = next_room_key
+        except KeyError:
+            print("***Please choose an available direction for this room.***")
+
+
--- src/room.py
+++ src/room.py
@@ -1,2 +1,31 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, name, description, n_to = None, e_to = None, s_to = None, w_to = None):
+        self.name = name
+        self.description = description
+        
+        self.n_to = n_to
+        self.e_to = e_to
+        self.s_to = s_to
+        self.w_to = w_to
+    
+    def enter(self, user_input):
+        if user_input == 'n':
+            return self.n_to
+        if user_input == 'e':
+            return self.e_to
+        if user_input == 's':
+            return self.s_to
+        if user_input == 'w':
+            return self.w_to
+
+    def get_available_directions(self):
+        available_directions = []
+
+        available_directions.append("'n' for North") if self.n_to != None else None
+        available_directions.append("'e' for East") if self.e_to != None else None
+        available_directions.append("'s' for South") if self.s_to != None else None
+        available_directions.append("'w' for West") if self.w_to != None else None
+
+        return available_directions
